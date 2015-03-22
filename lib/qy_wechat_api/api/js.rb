623b779cf4c04afbc119a129f01497cd9f43fc69
lanrion <huaitao-deng@foxmail.com>
2015-03-22 11:30:50 +0800

    添加js api

---
+++ lib/qy_wechat_api/api/js.rb
@@ -0,0 +1,27 @@
+module QyWechatApi
+  module Api
+    class Js < Base
+      def sign_package(url)
+        timestamp = Time.now.to_i
+        noncestr = SecureRandom.hex(16)
+        str = "jsapi_ticket=#{get_jsticket}&noncestr=#{noncestr}&timestamp=#{timestamp}&url=#{url}";
+        signature = Digest::SHA1.hexdigest(str)
+        {
+          "appId"     => corp_id,    "nonceStr"  => noncestr,
+          "timestamp" => timestamp, "url"       => url,
+          "signature" => signature, "rawString" => str
+        }
+      end
+
+      # https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=ACCESS_TOKE
+      def get_jsticket
+        cache_key = "jsticket-#{client.redis_key}"
+        Rails.cache.fetch(cache_key, expires_in: 7100.seconds) do
+          res = http_get(get_jsapi_ticket, {waive_base_url: true})
+          res.result["ticket"]
+        end
+      end
+
+    end
+  end
+end
